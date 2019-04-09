528e1076c3697937070bfef2ac20f016e31cb39e
Son Truong <sntruong12@gmail.com>
2019-04-09 17:40:24 -0500

    initial setup for user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: Number,
+    default: 1,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('user', userSchema);
