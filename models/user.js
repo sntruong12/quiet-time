9a4f2cfdd6b2c175aac1c05befa7bc2de30e2ca4
Son Truong <sntruong12@gmail.com>
2019-04-09 18:55:18 -0500

    add unique helper to username and email property in user schema

--- models/user.js
+++ models/user.js
@@ -5,10 +5,12 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: {
     type: String,
+    unique: true,
     required: true
   },
   email: {
     type: String,
+    unique: true,
     required: true
   },
   password: {
