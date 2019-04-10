61484dba6f784982ba9abb2d78bb08211cb9da64
Son Truong <sntruong12@gmail.com>
2019-04-09 22:16:41 -0500

    initial commit for LoginForm

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginForm = (props) => (
+  <div>
+    <label>email</label>
+    <input type="text"/>
+    <label>password</label>
+    <input type="text"/>
+    <button>sign in</button>
+  </div>
+);
+
+export default LoginForm;
