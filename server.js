f6634186202ff1fa609b5d2799084c65f7b23135
Son Truong <sntruong12@gmail.com>
2019-04-09 23:40:38 -0500

    set up cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,7 @@ const usersRouter = require('./routes/api/users');
 require('dotenv').config()
 require('./config/database');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
