1d734ff116dd424cc786ae208558689a94c69b3e
Son Truong <sntruong12@gmail.com>
2019-04-09 22:16:09 -0500

    move app.js to pages directory, change the import path for app.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './pages/App/App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router><App /></Router>,
