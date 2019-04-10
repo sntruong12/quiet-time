883bf56179e36d372861780e66cd835bafec8c12
Son Truong <sntruong12@gmail.com>
2019-04-09 22:37:09 -0500

    initial set up for signup page

---
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+
+const SignUpPage = (props) => (
+  <div>
+    <SignUpForm />
+  </div>
+)
+
+export default SignUpPage;
