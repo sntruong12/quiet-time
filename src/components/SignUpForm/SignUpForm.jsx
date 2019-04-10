0c404846229f315c923ab8b1f72a16ed4e18886c
Son Truong <sntruong12@gmail.com>
2019-04-10 16:40:42 -0500

    add state and input fields are updating state - ready for posting the state data to create a user

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,15 +1,72 @@
 import React from 'react';
 
-const SignUpForm = (props) => (
-  <div>
-    <label>username</label>
-    <input type="text" required/>
-    <label>email</label>
-    <input type="text" required/>
-    <label>password</label>
-    <input type="password" minlength="8" required/>
-    <button>sign up</button>
-  </div>
-);
+class SignUpForm extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+  };
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+
+    }
+    catch(err) {
+      
+    }
+  }
+
+  isFormValid() {
+    return !(this.state.username && this.state.email && this.state.password)
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>username</label>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            required
+          />
+          <label>email</label>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
+            required
+          />
+          <label>password</label>
+          <input
+            type="password"
+            minLength="8"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+            required
+          />
+          <button 
+            type="button"
+            disabled={this.isFormValid()}
+          >
+            sign up        
+          </button>
+        </form>
+      </div>
+    )
+  }
+} 
+
 
 export default SignUpForm;
