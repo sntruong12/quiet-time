5efc07c79d3e5a1343c99ea37760896ccce28836
Son Truong <sntruong12@gmail.com>
2019-04-11 00:58:24 -0500

    initial set up for getting all teachers and creating a teacher

---
+++ controllers/api/teachers.js
@@ -0,0 +1,42 @@
+const Teacher = require('../../models/teacher');
+
+const index = async (req, res) => {
+  try {
+    const teachers = await Teacher.find({}).then(res => res);
+    res.status(200)
+      .json(teachers)
+  }
+  catch(err) {
+    console.log(err)
+    res.status(500)
+      .json({
+        status: 500,
+        message: 'Unable to get all teachers',
+        error: err.errmsg
+      })
+  }
+}
+
+const create = async (req, res) => {
+  console.log('post teacher route hit')
+  const teacher = new Teacher(req.body)
+  try {
+    await teacher.save();
+    res.status(201)
+      .json(teacher)
+  }
+  catch(err) {
+    console.log(err)
+    res.status(500)
+      .json({
+        status: 500,
+        message: 'Unable to create new teacher',
+        error: err.errmsg
+      })
+  }
+}
+
+module.exports = {
+  index,
+  create
+}
