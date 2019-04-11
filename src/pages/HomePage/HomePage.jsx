e7bc432906d3a577cb653d6d85729383a9f5767d
Son Truong <sntruong12@gmail.com>
2019-04-10 23:57:23 -0500

    refactor code to render different components based on if the user is logged in

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import LoginButton from '../../components/LoginButton/LoginButton';
+import LogoutButton from '../../components/LogoutButton/LogoutButton';
 import SignUpButton from '../../components/SignUpButton/SignUpButton';
 
-const HomePage = (props) => (
-  <div>
-    <h1>quiet time</h1>
-    <LoginButton />
-    <SignUpButton />
-  </div>
-);
+const HomePage = (props) => {
+  //check if user is logged in and render components conditionally
+  let home = props.user ?
+    <LogoutButton />
+    :
+    <div>
+      <LoginButton />
+      <SignUpButton />
+    </div>
+  
+  return (
+    <div>
+      <h1>quiet time</h1>
+      {home}
+    </div>
+  );
+}
 
 export default HomePage;
