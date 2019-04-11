3e12708770b1524bc7f0767808d6635fb6b982ac
Son Truong <sntruong12@gmail.com>
2019-04-11 00:16:34 -0500

    pass prop for handlesignup to the signupform component

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -4,6 +4,7 @@ import SignUpForm from '../../components/SignUpForm/SignUpForm';
 const SignUpPage = (props) => (
   <div>
     <SignUpForm
+      handleSignUp={props.handleSignUp}
       history={props.history}
     />
   </div>
