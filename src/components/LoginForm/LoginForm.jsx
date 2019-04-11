ddd70514badd50f44a57913be2b6744cf82e77ad
Son Truong <sntruong12@gmail.com>
2019-04-11 17:28:05 -0500

    complete refactor to implement login feature

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,57 @@
 import React from 'react';
+import userService from '../../utils/userService';
 
-const LoginForm = (props) => (
-  <div>
-    <label>email</label>
-    <input type="text" required/>
-    <label>password</label>
-    <input type="password" required/>
-    <button>sign in</button>
-  </div>
-);
+class LoginForm extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      await userService.login(this.state)
+      // allows the <App> to know a user logged in
+      this.props.handleSignupOrLogin();
+      // routes the user to the home page
+      this.props.history.push('/');
+    }
+    catch(err) {
+      // implement some type of ui for error
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <form>
+          <label>email</label>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
+            required
+            />
+          <label>password</label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+            required
+            />
+          <button onClick={this.handleSubmit}>sign in</button>
+        </form>
+      </div>
+    );
+  }
+} 
 
 export default LoginForm;
