fb791e4f4b835c03f57ea5c0d658d36c0bd795db
Son Truong <sntruong12@gmail.com>
2019-04-11 17:22:54 -0500

    rename create to signup and initial set up for login controller

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,7 +1,7 @@
 const User = require('../../models/user');
 const jwt = require('jsonwebtoken');
 
-const create = async (req, res) => {
+const signup = async (req, res) => {
   const user = new User(req.body)
   try {
     await user.save();
@@ -20,6 +20,28 @@ const create = async (req, res) => {
   }
 }
 
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email
+    });
+    if(!user) return res.status(401).json({err: 'bad credentials'});
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(isMatch) {
+        const token = createJWT(user);
+        res.json({token});
+      } else {
+        return res.status(401)
+          .json({err: 'bad credentials'});
+      }
+    });
+  }
+  catch(err) {
+    res.status(401)
+      .json(err)
+  }
+}
+
 /*----- Helper Functions -----*/
 function createJWT(user) {
   return jwt.sign(
@@ -30,5 +52,6 @@ function createJWT(user) {
 }
 
 module.exports = {
-  create
+  signup,
+  login
 };
