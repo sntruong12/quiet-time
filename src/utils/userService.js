5043e4a06858c7c8a045d2925710fd24dbc7af00
Son Truong <sntruong12@gmail.com>
2019-04-11 17:28:44 -0500

    small url refactor in signup function and implement the login userservice function

--- src/utils/userService.js
+++ src/utils/userService.js
@@ -5,7 +5,7 @@ const BASE_URL = '/api/users/';
 const signup = async (user) => {
   try {
     let createdUser = await fetch(
-      BASE_URL,
+      BASE_URL + 'signup',
       {
         method: 'POST',
         headers: {
@@ -38,8 +38,34 @@ const logout = () => {
   tokenService.removeToken();
 }
 
+const login = async (creds) => {
+  try {
+    let user = await fetch(
+      BASE_URL + 'login',
+      {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(creds)
+      }
+    )
+    .then(res => {
+      // Valid login if we have a status of 2xx (res.ok)
+      if (res.ok) return res.json();
+      throw new Error('Bad Credentials!');
+    })
+    .then(({token}) => tokenService.setToken(token));
+    return user
+  }
+  catch(err) {
+    // some ui for error on log in attempt
+  }
+}
+
 export default {
   signup,
   getUser,
   logout,
+  login
 };
