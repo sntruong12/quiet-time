2464e30d4223de344d97ccdcade1564bc0924f44
Son Truong <sntruong12@gmail.com>
2019-04-11 15:30:12 -0500

    add quotes subschema on the teacher model

--- models/teacher.js
+++ models/teacher.js
@@ -2,12 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const quoteSchema = new Schema({
+  text: {
+    type: String,
+    unique: true,
+    require: true
+  }
+}, {
+  timestamps: true
+})
+
 const teacherSchema = new Schema({
   name: {
     type: String,
     unique: true,
     required: true
-  }
+  },
+  quotes: [quoteSchema]
 }, {
   timestamps: true
 });
