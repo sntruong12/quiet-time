269599645cc95adb467dc0f494f5a94d0b9ce429
Son Truong <sntruong12@gmail.com>
2019-04-10 22:43:04 -0500

    set up state to pick up on the user logged in

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,10 +3,30 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import LoginPage from '../LoginPage/LoginPage';
 import SignUpPage from '../SignUpPage/SignUpPage';
-
+import userService from '../../utils/userService';
 import './App.css';
 
 class App extends Component {
+  state = {
+    ...this.getInitialState(),
+    user: null
+  }
+
+  getInitialState() {
+    return {
+
+    }
+  }
+
+  /*----- Lifecycle Methods -----*/
+
+  async componentDidMount() {
+    const user = userService.getUser();
+    this.setState({
+      user,
+    })
+  }
+
   render() {
     return (
       <div className="App">
