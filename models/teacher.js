3dab9f7a03fedd248826f86b455bbc90a7eeda48
Son Truong <sntruong12@gmail.com>
2019-04-11 00:38:40 -0500

    initial set up for teacher schema

---
+++ models/teacher.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const teacherSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('teacher', teacherSchema);
