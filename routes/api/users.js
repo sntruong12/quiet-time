76886dbd5dd0943b5adfcc906d8bb86d909cd6e5
Son Truong <sntruong12@gmail.com>
2019-04-11 17:24:01 -0500

    rename create route to signup and add login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 
-router.post('/users', usersCtrl.create);
+router.post('/users/signup', usersCtrl.signup);
+router.post('/users/login', usersCtrl.login)
 
 module.exports = router;
