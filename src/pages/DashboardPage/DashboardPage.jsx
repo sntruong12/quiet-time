b7fc04fbf4457c990e33490fc8eabb625d4bde0b
Son Truong <sntruong12@gmail.com>
2019-04-12 01:40:03 -0500

    initial commit for this page

---
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dashboard from '../../components/Dashboard/Dashboard';
+
+const DashboardPage = (props) => (
+  <div>
+    <Dashboard />
+  </div>
+);
+
+export default DashboardPage;
