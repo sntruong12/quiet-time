8f11041ee61a4b549c2f9486f0341d10dcd9e198
Son Truong <sntruong12@gmail.com>
2019-04-12 12:28:53 -0500

    add mode cors in fetch call

--- src/utils/teacherService.js
+++ src/utils/teacherService.js
@@ -3,7 +3,7 @@ const BASE_URL = '/api/teachers/';
 // returns an array of all the teachers name
 const getAllTeachers = async () => {
   let teacherNames = [];
-  let teachers = await fetch(BASE_URL).then(res => res.json()).then(data => data);
+  let teachers = await fetch(BASE_URL, {mode: "cors"}).then(res => res.json()).then(data => data);
   teachers.forEach(t => {
     teacherNames.push(t.name)
   });
