62fb09670f73376beed6113280b4a09913b2f09e
Son Truong <sntruong12@gmail.com>
2019-04-14 18:48:48 -0500

    small refactor to push an a string if async fetch fails

--- src/utils/teacherService.js
+++ src/utils/teacherService.js
@@ -3,11 +3,18 @@ const BASE_URL = '/api/teachers/';
 // returns an array of all the teachers name
 const getAllTeachers = async () => {
   let teacherNames = [];
-  let teachers = await fetch(BASE_URL, {mode: "cors"}).then(res => res.json()).then(data => data);
-  teachers.forEach(t => {
-    teacherNames.push(t.name)
-  });
-  return teacherNames;
+  try {
+    let teachers = await fetch(BASE_URL, {mode: "cors"}).then(res => res.json()).then(data => data);
+    teachers.forEach(t => {
+      teacherNames.push(t.name)
+    });
+    return teacherNames;
+  }
+  catch(err) {
+    console.log(err);
+    teacherNames.push('much emptiness in our soul');
+    return teacherNames;
+  }
 }
 
 export default {
