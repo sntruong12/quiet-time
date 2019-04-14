67796bd05ecccdfe763b09f55dcaa8982978b33b
Son Truong <sntruong12@gmail.com>
2019-04-14 18:49:27 -0500

    initial commit to get all quotes from one teacher

---
+++ src/utils/quotesService.js
@@ -0,0 +1,21 @@
+const BASE_URL = '/api/teachers/';
+
+const getQuotes = async (t_name) => {
+  let allQuotes = [];
+  try {
+    let quotes = await fetch(BASE_URL + t_name + '/quotes', {mode: 'cors'}).then(res => res.json()).then(data => data);
+    quotes.forEach(q => {
+      allQuotes.push(q.text)
+    });
+    return allQuotes;
+  }
+  catch(err) {
+    console.log(err)
+    allQuotes.push('Error - Why not?')
+    return allQuotes
+  }
+}
+
+export default {
+  getQuotes
+}
