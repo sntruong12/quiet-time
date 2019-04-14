5d6f5a56d10e23e85b5626b7dcc46ae43523bca3
Son Truong <sntruong12@gmail.com>
2019-04-14 18:53:43 -0500

    initial commit for meditation page

---
+++ src/pages/MeditationPage/MeditationPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Meditation from '../../components/Meditation/Meditation';
+
+const MeditationPage = (props) => (
+  <div>
+    <Meditation />
+  </div>
+);
+
+export default MeditationPage;
