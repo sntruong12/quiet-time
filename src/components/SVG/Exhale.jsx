6b69e11e83532a07270ba0c74aad8b5051e4653c
Son Truong <sntruong12@gmail.com>
2019-04-14 21:42:51 -0500

    change stroke-width attribute to strokeWidth

--- src/components/SVG/Exhale.jsx
+++ src/components/SVG/Exhale.jsx
@@ -12,7 +12,7 @@ const Exhale = () => (
     <defs>
       <path d="M340.95 320c2.41-27.46 10.45-48.36 24.13-62.7 13.68-14.34 32.99-22.11 57.94-23.33 27.06.55 47.43 8.33 61.11 23.33 13.67 15 20.66 37.22 20.95 66.67-.29 19.13-7.28 35.53-20.95 49.2-13.68 13.68-34.05 24.63-61.11 32.86h-288.1" id="exhale"/>
     </defs>
-    <use xlinkHref="#exhale" fillOpacity="0" stroke="#FFF" stroke-width="15"/>
+    <use xlinkHref="#exhale" fillOpacity="0" stroke="#FFF" strokeWidth="15"/>
   </svg>
 )
 
--- src/components/SVG/Hold.jsx
+++ src/components/SVG/Hold.jsx
@@ -12,7 +12,7 @@ const Hold = () => (
     <defs>
       <path d="M249.37 253.33c-25 25-29.37 32.15-29.37 66.67s4.37 46.43 29.37 71.43c25 25 34.52 28.57 70.63 28.57 36.11 0 48.81-3.57 73.81-28.57S420 354.52 420 320c0-34.52-1.19-41.67-26.19-66.67S356.11 220 320 220s-45.63 8.33-70.63 33.33z" id="hold"/>
     </defs>
-    <use xlinkHref="#hold" fillOpacity="0" stroke="#FFF" stroke-width="10"/>
+    <use xlinkHref="#hold" fillOpacity="0" stroke="#FFF" strokeWidth="10"/>
   </svg>
 )
 
