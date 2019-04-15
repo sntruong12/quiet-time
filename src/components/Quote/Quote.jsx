d79f80845467dc50beb0abb572dfd76efe593d70
Son Truong <sntruong12@gmail.com>
2019-04-15 12:40:12 -0500

    add feature to serve the quote

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -12,13 +12,34 @@ class Quote extends React.Component {
   }
 
   componentDidMount() {
+    // gets random quote from selected teacher
     this.props.handleRandomQuote();
+
+    this.interval = setTimeout(() => {
+      this.setState({
+        renderInitialMessage: false,
+        renderFinalMessage: true
+      })
+    }, 10000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
     return (
       <div>
-        Quotes!
+        { this.state.renderInitialMessage ?
+          <h3>focus your energy child in this next message.</h3>
+          :
+          null
+        }
+        { this.state.renderFinalMessage ?
+          <p>{this.props.quote}</p>  
+          :
+          null
+        }
       </div>
     )
   }
