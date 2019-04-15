e8c0d83cb318e9ba517ab53c0cf1d7cc75b48a6f
Son Truong <sntruong12@gmail.com>
2019-04-15 01:16:38 -0500

    change animation time and add h3 tag

--- src/components/SVG/Inhale.jsx
+++ src/components/SVG/Inhale.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 import './Inhale.css';
 
 const Inhale = () => (
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  xmlnsXlink="http://www.w3.org/1999/xlink"
-  viewBox="124.698 246.366 359.962 232.542"
-  width="200" height="200">
-    <defs>
-      <path d="M129.7 251.37l200.91 200.9c20.7 15.32 41.21 22.51 61.5 21.55 20.3-.95 40.39-10.04 60.28-27.27 18.2-20.86 27.29-40.95 27.27-60.27-.02-19.33-9.15-37.88-27.39-55.67-16.23-16.69-34.04-25.07-53.42-25.14-19.38-.08-40.33 8.15-62.86 24.69" id="inhale"/>
-    </defs>
-    <use xlinkHref="#inhale" fillOpacity="0" stroke="#FFF" strokeWidth="15"/>
-  </svg>
+  <div className="inhale">
+    <h3>inhale for 8 seconds</h3>
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="124.698 246.366 359.962 232.542"
+    width="200" height="200">
+      <defs>
+        <path d="M129.7 251.37l200.91 200.9c20.7 15.32 41.21 22.51 61.5 21.55 20.3-.95 40.39-10.04 60.28-27.27 18.2-20.86 27.29-40.95 27.27-60.27-.02-19.33-9.15-37.88-27.39-55.67-16.23-16.69-34.04-25.07-53.42-25.14-19.38-.08-40.33 8.15-62.86 24.69" id="inhale"/>
+      </defs>
+      <use xlinkHref="#inhale" fillOpacity="0" stroke="#FFF" strokeWidth="15"/>
+    </svg>
+  </div>
 )
 
 export default Inhale;
