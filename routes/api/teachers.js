9f3b19de12d92702d0be1b29276f3b84bc454e90
Son Truong <sntruong12@gmail.com>
2019-04-15 02:40:11 -0500

    add route to delete one teacher

--- routes/api/teachers.js
+++ routes/api/teachers.js
@@ -5,5 +5,6 @@ const teachersCtrl = require('../../controllers/api/teachers');
 router.get('/teachers', teachersCtrl.index);
 router.get('/teachers/:t_name', teachersCtrl.show);
 router.post('/teachers', teachersCtrl.create);
+router.delete('/teachers/:t_name', teachersCtrl.delete);
 
 module.exports = router;
