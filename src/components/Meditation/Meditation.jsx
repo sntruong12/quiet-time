c66ec67986240a0c0748b1f4b39a89f05d5149e9
Son Truong <sntruong12@gmail.com>
2019-04-15 01:18:06 -0500

    set up animation interval- page now renders SVG animations based on set intervals

--- src/components/Meditation/Meditation.jsx
+++ src/components/Meditation/Meditation.jsx
@@ -1,28 +1,70 @@
 import React from 'react';
-import quotesService from '../../utils/quotesService';
 import Inhale from '../SVG/Inhale';
 import Hold from '../SVG/Hold';
 import Exhale from '../SVG/Exhale';
 
 class Meditation extends React.Component {
-  state = {
-    quotes: []
+  constructor() {
+    super();
+    this.state = {
+      renderInitialMessage: true,
+      renderInhale: false,
+      renderHold: false,
+      renderExhale: false,
+      renderFinalMessage: false
+    }
+    
+    this.interval = null
   }
 
   /*----- Lifecycle Methods -----*/
   async componentDidMount() {
-    const quotes = await quotesService.getQuotes('SonYungMoon');
-    this.setState({
-      quotes,
-    })
+    this.interval = setTimeout(() => {
+      this.setState({
+        renderInitialMessage: false,
+        renderInhale: true
+      })
+    }, 8000)
+    this.interval = setTimeout(() => {
+      this.setState({
+        renderInhale: false,
+        renderHold: true
+      })
+    }, 16500)
+    this.interval = setTimeout(() => {
+      this.setState({
+        renderHold: false,
+        renderExhale: true
+      })
+    }, 24500)
+    this.interval = setTimeout(() => {
+      this.setState({
+        renderExhale: false,
+        renderFinalMessage: true
+      })
+    }, 34500)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
     return (
       <div>
-        <Inhale />
-        <Hold />
-        <Exhale />
+        { this.state.renderInitialMessage ?
+          <h3>let's meditate</h3> 
+          :
+          null
+        }
+        { this.state.renderInhale ? <Inhale /> : null }
+        { this.state.renderHold ? <Hold /> : null }
+        { this.state.renderExhale ? <Exhale /> : null }
+        { this.state.renderFinalMessage ?
+          <h3>feel focus grasshopper?</h3> 
+          :
+          null
+        }
       </div>
     )
   }
