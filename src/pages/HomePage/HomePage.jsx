a3e37154397463c1bb2b6224a797fe5fd675d78a
Son Truong <sntruong12@gmail.com>
2019-04-16 02:12:23 -0500

    change wording on tooltip

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -26,7 +26,7 @@ const HomePage = (props) => {
     <div className="homepage-container">
       <h1>quiet time</h1>
       <div>
-        <Tooltip title="quiet time is an app focused to get you in the right mindset for growth. We provide an environment for you to let go of the otherworldly distractions through breathing exercises. This will put you in a place to focus on an idea. At this point, we share with you a meaningful quote from a thought leader. The end goal is provide you a space and opportunity to be introspective." placement="top">
+        <Tooltip title="quiet time is an app focused to get you in the right mindset for growth. We provide an environment for you to let go of the otherworldly distractions through breathing exercises. This will put you in a place to focus on an idea. Through a minimalistic experience, we share with you a meaningful quote from a thought leader. The end goal is provide you a space and opportunity to be introspective." placement="top">
           <InfoIcon />
         </Tooltip>
       </div>
