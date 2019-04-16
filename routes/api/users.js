ab38e168558a49d7faea843edec705bb727c8d84
Son Truong <sntruong12@gmail.com>
2019-04-15 22:03:46 -0500

    add delete and index route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 
+router.get('/users', usersCtrl.index);
 router.post('/users/signup', usersCtrl.signup);
-router.post('/users/login', usersCtrl.login)
-router.put('/users/:email/addlevel', usersCtrl.addlevel)
+router.post('/users/login', usersCtrl.login);
+router.put('/users/:email/addlevel', usersCtrl.addlevel);
+router.delete('/users/:email', usersCtrl.delete);
 
 module.exports = router;
