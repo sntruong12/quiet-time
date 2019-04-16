3157980a6a62fdc10466f67532f2af9d835f41e2
Son Truong <sntruong12@gmail.com>
2019-04-16 02:05:27 -0500

    add tooltip and small markup changes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import teacherService from '../../utils/teacherService';
+import Tooltip from '@material-ui/core/Tooltip';
+import './Dashboard.css'
 
 class Dashboard extends React.Component {
   state = {
@@ -17,12 +19,18 @@ class Dashboard extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Welcome {this.props.user.username}</h1>
+      <div className="dashboard-div">
+        <h1>
+          welcome <Tooltip title={`He/She/Non-Binary-Pronoun who goes by ${this.props.user.username}`} placement="top"><span>grasshopper</span></Tooltip>
+        </h1>
+        <hr/>
+        <h5>select a sensei below to learn from</h5>
         {this.state.teachers.map(t => (
-          <Link to="/meditation">
-            <p onClick={this.props.handleTeacherClick}>{t}</p>
-          </Link>
+          <div>
+            <Link className="dashboard-link" to="/meditation">
+              <h3 onClick={this.props.handleTeacherClick}>{t}</h3>
+            </Link>
+          </div>
         ))}
       </div>
     )
