cd6cb047cb763bf587e889d6a75414e881f37f1a
Son Truong <sntruong12@gmail.com>
2019-04-15 19:21:08 -0500

    add middleware to protect server routes

--- routes/api/quotes.js
+++ routes/api/quotes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const quotesCtrl = require('../../controllers/api/quotes');
 
-router.get('/:t_name/quotes', quotesCtrl.index);
 router.post('/:t_name/quotes', quotesCtrl.create);
+
+// Protected Routes
+router.use(require('../../config/auth'));
+router.get('/:t_name/quotes', quotesCtrl.index);
 
 module.exports = router;
--- routes/api/teachers.js
+++ routes/api/teachers.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const teachersCtrl = require('../../controllers/api/teachers');
 
-router.get('/teachers', teachersCtrl.index);
 router.get('/teachers/:t_name', teachersCtrl.show);
 router.post('/teachers', teachersCtrl.create);
 router.delete('/teachers/:t_name', teachersCtrl.delete);
+
+// Protected Routes
+router.use(require('../../config/auth'));
+router.get('/teachers', teachersCtrl.index);
 
 module.exports = router;
